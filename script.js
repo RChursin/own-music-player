fe93a60da920c3fc58f4885999e8ed0d370ac6fc
ganstacoder <ganstacoder@gmail.com>
2023-05-08 21:21:26 -0700

    Play and Pause Func

---
+++ script.js
@@ -0,0 +1,32 @@
+const music = document.querySelector("audio");
+const img = document.querySelector("img");
+const play = document.getElementById("play");
+const artist = document.getElementById("artist");
+const title = document.getElementById("title");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+// Check Playing Function
+let isPlaying = false;
+
+
+// Play Music Function
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+// Pause Music Function
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+
