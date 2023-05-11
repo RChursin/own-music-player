9e34ee0736f197bb632761cc47dcc2c35d2f5f54
ganstacoder <ganstacoder@gmail.com>
2023-05-10 20:58:39 -0700

    Add button control funcs

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const music = document.querySelector("audio");
-const img = document.querySelector("img");
-const play = document.getElementById("play");
-const artist = document.getElementById("artist");
+const image = document.querySelector("img");
 const title = document.getElementById("title");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "One_Day",
+    displayName: "One Day",
+    artist: "Matisyahu",
+  },
+  {
+    name: "Searchin",
+    displayName: "Searchin",
+    artist: "Matisyahu",
+  }
+];
+
 
 // Check Playing Function
 let isPlaying = false;
@@ -28,5 +43,47 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `images/${song.name}.jpeg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+
+// Previous Song Function
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song Function
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+
 
 
